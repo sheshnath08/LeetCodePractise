d75b11cb5651574fb0bfccf572f18152709fa873
sheshnath08 <sheshnathyadav08@gmail.com>
2017-05-11 00:44:58 -0400

    MedianFinder

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This repository contains solutions of the leetcode problems
 * SpiralMatrixII.java contains solution for [Spiral Matrix II](https://leetcode.com/problems/spiral-matrix-ii/)
 * RegularRexpressionMatching.java contains solution for [Regular Expression Matching](https://leetcode.com/problems/regular-expression-matching/#/description) 
 * [Find Median from Data Stream](https://leetcode.com/problems/find-median-from-data-stream/#/description)
+* RotateImage.java contains solution for [Rotate Image](https://leetcode.com/problems/rotate-image/)
--- src/MedianFinder.java
+++ src/MedianFinder.java
@@ -1,6 +1,36 @@
 /**
  * Created by sheshnath on 5/10/2017.
  */
+import java.util.*;
 public class MedianFinder {
 
+    /** initialize your data structure here. */
+    PriorityQueue<Integer> minHeap;
+    PriorityQueue<Integer> maxHeap;
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+
+    public void addNum(int num) {
+            maxHeap.add(num);
+            minHeap.add(maxHeap.poll());
+            if(maxHeap.size()<minHeap.size()){
+                maxHeap.add(minHeap.poll());
+            }
+    }
+
+    public double findMedian() {
+        if(maxHeap.size() == minHeap.size()){
+            return (maxHeap.peek()+minHeap.peek())/2.0;
+        }
+        return maxHeap.peek();
+    }
 }
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder obj = new MedianFinder();
+ * obj.addNum(num);
+ * double param_2 = obj.findMedian();
+ */
